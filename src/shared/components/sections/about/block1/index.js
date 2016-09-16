e4c7540099471d295522be3009a17b5a65f82734
jaime <garciadiazjaime@gmail.com>
2016-09-16 18:11:11 -0500

    block 1

--- src/shared/components/sections/about/block1/index.js
+++ src/shared/components/sections/about/block1/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
-
+import SVG from '../../../svg';
+const style = require('./style.scss');
 
 export default class Block1 extends React.Component {
   render() {
-    return (<div>bloque1about</div>);
+    return (<div className={style.block1}>
+      <div className={style.icon2} />
+      <div className={style.title5}>
+        CÁMARA GESELL Y <br />
+        FOCUS GROUPS
+      </div>
+      <div className={style.icon4}>
+        <SVG network="arrow_down" />
+      </div>
+    </div>);
   }
 }
