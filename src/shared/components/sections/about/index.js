5215da6d6d84f5395c6a083c65cd3e8da2499cef
jaime <garciadiazjaime@gmail.com>
2016-09-17 11:27:57 -0500

    block3

--- src/shared/components/sections/about/index.js
+++ src/shared/components/sections/about/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Block1 from './block1';
 import Block2 from './block2';
+import Block3 from './block3';
 
 export default class AboutSection extends React.Component {
   render() {
     return (<div>
       <Block1 />
       <Block2 />
+      <Block3 />
     </div>);
   }
 }
