b0ad26f743d1ed78ab072631e0e901157e6f6639
jaime <garciadiazjaime@gmail.com>
2016-09-19 22:36:30 -0500

    block6

---
+++ src/shared/components/blocks/block2/index.js
@@ -0,0 +1,83 @@
+/* eslint max-len: [2, 500, 4] */
+import React from 'react';
+import _ from 'lodash';
+import Slider from 'react-slick';
+
+const style = require('./style.scss');
+
+export default class Block2 extends React.Component {
+
+  renderItems(data) {
+    if (_.isArray(data) && data.length) {
+      return data.map((item, index) => {
+        return (<div key={index}>
+          <img src={item.image} />
+        </div>);
+      });
+    }
+    return null;
+  }
+
+  render() {
+    const { data } = this.props;
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      responsive: [{
+        breakpoint: 420,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      }, {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 2,
+        },
+      }, {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 4,
+        },
+      }, {
+        breakpoint: 100000,
+        settings: {
+          slidesToShow: 6,
+          slidesToScroll: 5,
+        },
+      }],
+    };
+    return (<div className={style.block6}>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-sm-4">
+            <div className={style.title8}>
+              NUESTRA EXPERIENCIA
+            </div>
+          </div>
+          <div className="col-sm-8">
+            <div className={style.title8}>
+              Hemos colaborado con las mejores agencias de investigación
+              de mercados a nivel nacional.
+            </div>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className={style.slider}>
+            <Slider {...settings}>
+            {this.renderItems(data)}
+            </Slider>
+          </div>
+        </div>
+      </div>
+    </div>);
+  }
+}
+
+Block2.propTypes = {
+  data: React.PropTypes.array.isRequired,
+};
---
+++ src/shared/components/sections/services/block6/data.js
@@ -0,0 +1,43 @@
+export default [
+  {
+    image: '/images/apoyo-logisitico/slider/logo-acimsa.jpg',
+  }, {
+    image: '/images/apoyo-logisitico/slider/Logo-berumen.jpg',
+  }, {
+    image: '/images/apoyo-logisitico/slider/logo-brain.jpg',
+  }, {
+    image: '/images/apoyo-logisitico/slider/logo-elinstituto.jpg',
+  }, {
+    image: '/images/apoyo-logisitico/slider/logo-evidens.jpg',
+  }, {
+    image: '/images/apoyo-logisitico/slider/logo-gfk.jpg',
+  }, {
+    image: '/images/apoyo-logisitico/slider/logo-inmega.jpg',
+  }, {
+    image: '/images/apoyo-logisitico/slider/logo-inmersa.jpg',
+  }, {
+    image: '/images/apoyo-logisitico/slider/logo-ipsos.jpg',
+  }, {
+    image: '/images/apoyo-logisitico/slider/logo-kp.jpg',
+  }, {
+    image: '/images/apoyo-logisitico/slider/logo-marcaei.jpg',
+  }, {
+    image: '/images/apoyo-logisitico/slider/logo-mb.jpg',
+  }, {
+    image: '/images/apoyo-logisitico/slider/logo-more.jpg',
+  }, {
+    image: '/images/apoyo-logisitico/slider/logo-nodo.jpg',
+  }, {
+    image: '/images/apoyo-logisitico/slider/logo-ovalbox.jpg',
+  }, {
+    image: '/images/apoyo-logisitico/slider/logo-parametria.jpg',
+  }, {
+    image: '/images/apoyo-logisitico/slider/logo-serta.jpg',
+  }, {
+    image: '/images/apoyo-logisitico/slider/logo-sigmados.jpg',
+  }, {
+    image: '/images/apoyo-logisitico/slider/logo-tns.jpg',
+  }, {
+    image: '/images/apoyo-logisitico/slider/logo-wisdom.jpg',
+  },
+];
